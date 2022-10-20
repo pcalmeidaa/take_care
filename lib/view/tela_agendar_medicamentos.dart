db44b112e40a45b1d9c6cd559e14f12dada233a1
Richard Barros <richardbarros@duck.com>
2022-10-20 14:02:18 -0300

    Agendamento de medicamentos

---
+++ lib/view/tela_agendar_medicamentos.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+// import 'package:app_agenda/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class TelaAgendaMedicacoes extends StatefulWidget {
+  // final Contact contact;
+
+  // TelaAgendaMedicacoes({this.contact});
+
+  @override
+  _TelaAgendaMedicacoesState createState() => _TelaAgendaMedicacoesState();
+}
+
+class _TelaAgendaMedicacoesState extends State<TelaAgendaMedicacoes> {
+  final _nameController = TextEditingController();
+  final _doseController = TextEditingController();
+  final _periodoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: _requestPop,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text(
+              "Agendar medicamento",
+              style: TextStyle(color: Colors.white),
+            ),
+            centerTitle: true,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // Navigator.pop(context);
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.blue,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(),
+                  ),
+                  onTap: () {},
+                ),
+                campoText(FaIcon(FontAwesomeIcons.fileSignature), 'Nome',
+                    _nameController),
+                campoText(
+                    FaIcon(FontAwesomeIcons.flask), 'Dose', _doseController),
+                campoText(FaIcon(FontAwesomeIcons.calendar), 'Período',
+                    _periodoController),
+                // campoDropDown()
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Future<bool> _requestPop() {
+    return Future.value(true);
+  }
+
+  campoText(prefixIcon, hint, variavel) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: TextFormField(
+        controller: variavel,
+        style: GoogleFonts.montserrat(
+            textStyle: TextStyle(
+          fontSize: 15,
+        )),
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+            prefixIcon: Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              child: prefixIcon,
+            ),
+            hintText: hint,
+            hintStyle: TextStyle(
+              fontSize: 15,
+              color: Colors.grey,
+            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(5))),
+      ),
+    );
+  }
+}
